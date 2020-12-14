a6f6526418c1bd85037dfe14d1bff8248b19dde7
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-12-13 20:06:21 -0500

    create Login and LoginForm

--- resources/js/react-app-project/App.js
+++ resources/js/react-app-project/App.js
@@ -5,12 +5,14 @@ import About from './pages/about/About';
 import Build from './pages/build/Build';
 import Buy from './pages/buy/Buy';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 import Sell from './pages/sell/Sell';
 
 /* Anything that has register is temporary */
 import Register from './pages/register/Register';
 import CreateAccount from './pages/createAccount/CreateAccount';
+
 
 
 const App = () => {
@@ -25,6 +27,7 @@ const App = () => {
                 <Route exact path='/about' component={About} />
                 <Route exact path='/profile' component={Profile} />
                 <Route exact path='/create-account' component={CreateAccount} />
+                <Route exact path='/login' component={Login} />
 
                 <Route exact path='/register' component={Register} />
             </Router>
