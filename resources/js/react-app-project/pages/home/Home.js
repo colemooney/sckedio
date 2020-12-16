90f7ed0517d4cc04503fcc53aaa645d49ed772a9
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-12-16 17:19:04 -0500

    protect front end routes. begin JWT work

--- resources/js/react-app-project/pages/home/Home.js
+++ resources/js/react-app-project/pages/home/Home.js
@@ -5,20 +5,7 @@ import axios from 'axios';
 const Home = () => {
 
     useEffect(()=>{
-        // console.log('runs');
-        getUsers();
     }, []);
-
-    const getUsers = () => {
-        axios.get('api/users')
-            .then(res=>{
-                console.log(res);
-                console.log(res.data[0].username)
-            })
-            .catch(err=>{
-                console.log(err);
-            });
-    };
 
     return (
         <div>
