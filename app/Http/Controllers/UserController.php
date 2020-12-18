a8da6ce027fb880f8e1d22f5347607ed70c3f83f
MigsCortez <migueldlcortez@gmail.com>
2020-12-17 22:01:43 -0800

    UserController show function returns uname and email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    
+
+    public function show(Request $request) {
+        $user = Auth::user();
+
+        return response()->json([
+            'username' => $user->username,
+            'email' => $user->email
+        ], 200);
+    }
+
 }
