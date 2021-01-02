db67c68ce4f6d0a803c9acd63e175d3fed06125e
MigsCortez <migueldlcortez@gmail.com>
2021-01-02 14:00:59 -0800

    MODIFIED: Returning selected routes instead of giving all routes from passport.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,9 +26,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         
-        Passport::routes();
+        Passport::routes(function ($router){
+            $router->forAccessTokens();
+            $router->forPersonalAccessTokens();
+            $router->forTransientTokens();
+        });
         
-        Passport::refreshTokensExpireIn(now()->addYears(1));
+        Passport::refreshTokensExpireIn(now()->addDays(10));
         Passport::tokensExpireIn(now()->addMinutes(15));
     }
 }
