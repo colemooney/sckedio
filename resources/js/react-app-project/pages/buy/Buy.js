9cd8346fc6ca1d3a25d941d093f6b3d86fa6a508
Jarrett Dougherty <jarrettdougherty@gmail.com>
2021-01-07 22:59:21 -0500

    add design card. start layout on buy.

--- resources/js/react-app-project/pages/buy/Buy.js
+++ resources/js/react-app-project/pages/buy/Buy.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import NavBar from '../../components/navBar/NavBar';
+import DesignCard from '../../components/designCard/DesignCard';
+
+const fakeProducts = [
+    {
+        seller: "Jim's Designs",
+        productTitle: 'Custom Jacket',
+        interest: 3,
+        image: 'https://picsum.photos/225/280'
+    },
+    {
+        seller: "Kate's Designs",
+        productTitle: 'Dog Bed',
+        interest: 20,
+        image: 'https://picsum.photos/225/280'
+    },
+    {
+        seller: "Tim's Guitars",
+        productTitle: '5 String Guitar',
+        interest: 1,
+        image: 'https://picsum.photos/225/280'
+    },
+    {
+        seller: "Crazy Shirts",
+        productTitle: 'A Crazy Shirt',
+        interest: 13,
+        image: 'https://picsum.photos/225/280'
+    },
+]
 
 const Buy = (props) => {
     return (
         <div>
             <NavBar loggedIn={props.loggedIn} handleLogout={props.handleLogout} />
-            <h1>Buy Page</h1>
+            <Container>
+                <h1>Buy Page</h1>
+                <Grid container spacing={3}>
+                    {fakeProducts.map((product, i) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={i} align='center'>
+                            <DesignCard
+                                product={product}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </div>
     );
 };
