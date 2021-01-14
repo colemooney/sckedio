392bee4219b179255a131025c0bb941119237a89
Patrick Urbankowski <purbankowski@gmail.com>
2021-01-14 03:18:47 -0500

    added middle section skeleton of home page

--- resources/js/react-app-project/pages/home/Home.js
+++ resources/js/react-app-project/pages/home/Home.js
@@ -14,7 +14,7 @@ const Home = (props) => {
 
     const styles = {
         topSectionBackground: {
-            height: 800,
+            height: 1000,
             backgroundImage: `url(${"../../images/pexels-christina-morillo-1181345.jpg"})`,
             backgroundSize: 'cover',
             backgroundPosition: 'top-center',
@@ -23,6 +23,15 @@ const Home = (props) => {
             // width: "100%",
             // margin: 0,
             padding: 0,
+        },
+        smallWhiteboardImage: {
+            borderColor: 'transparent'
+        },
+        middleSectionBackground: {
+            padding: 50,
+        },
+        artisanWhiteboardImage: {
+            maxWidth: 600
         }
     }
 
@@ -32,7 +41,10 @@ const Home = (props) => {
             {/* <h1>Home Page</h1> */}
             <div>
                 <Container style={{ margin: 0, padding: 0 }}>
+
                     <Grid container spacing={0} direction="column" >{/*All page content */}
+
+                        {/*Top section */}
                         <Grid item style={styles.topSectionBackground}>{/*Photo background and container of top panel of marketplace text+smaller image*/}
                             <Box m={4}>
                                 <Grid item container xs={12}>{/*Container of "The marketplace..." text and image on right */}
@@ -42,20 +54,40 @@ const Home = (props) => {
                                     </Grid>
                                     <Grid item xs={12} md={6}>{/* Small image of man at whiteboard*/}
                                         <Box display="flex" justifyContent="center" alignItems="center" backgroundSize="contain">
-                                            <img src={require("../../images/pexels-christina-morillo-1181345.jpg")} id="whiteboard-small" width="100%" border="100px" alt="whiteboard-small" />
+                                            <img src={require("../../images/pexels-christina-morillo-1181345.jpg")} style={styles.smallWhiteboardImage} id="whiteboard-small-image" width="100%" border="100px" alt="whiteboard-small-image" />
                                         </Box>
                                     </Grid>
                                 </Grid>
                             </Box>
 
                             <Box m={4}>
-                                <Grid item container xs={12}>
+                                <Grid item container xs={12} >
                                     <Grid item xs={12} md={6}>
                                         <Typography variant='h3'>Your ideas into action.</Typography>
                                         <Typography variant='h5'>Sckedio connects people who have an idea for a product with people who want to build and buy their idea.</Typography>
                                     </Grid>
                                 </Grid>
                             </Box>
+                        </Grid>
+
+                        {/*Middle section*/}
+                        <Grid item style={styles.middleSectionBackground}>
+
+                            <Grid item container>{/*Container of "The marketplace..." text and image on right */}
+            
+                                <Grid item xs={12} md={6}>{/* Image of artisan drawing on whiteboard*/}
+                                    <Box display="flex" justifyContent="center" alignItems="center" backgroundSize="contain">
+                                        <img src={require("../../images/artisan.jpg")} style={styles.artisanWhiteboardImage} id="artisan-image" width="100%" alt="artisan-image" />
+                                    </Box>
+                                </Grid>
+
+                                <Grid item xs={12} md={6}>{/*The marketplace for ideas...+Sckedio connects... */}
+                                    <Typography variant='h2'>Turn your idea into a reality and make money in the process.</Typography>
+                                    <Typography variant='h5'>Any idea — fully developed or a shower thought— gives you a chance to make money</Typography>
+                                    <Typography variant='h5'>Follow a few easy steps to change the world with your idea</Typography>
+                                </Grid>
+
+                            </Grid>
 
                         </Grid>
 
