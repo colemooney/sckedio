a6c6c4186c5ed0468968948a8b064d4ad0e1fff0
Jarrett Dougherty <jarrettdougherty@gmail.com>
2021-02-06 14:33:28 -0500

    add state to getstarted. fix errors in home and getstarted

--- resources/js/react-app-project/pages/getStarted/GetStarted.js
+++ resources/js/react-app-project/pages/getStarted/GetStarted.js
@@ -11,6 +11,14 @@ import GetStartedForm from '../../components/getStartedForm/GetStartedForm';
 import axios from 'axios';
 
 const GetStarted = (props) => {
+    const [ideaName, setIdeaName] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [productCategory, setProductCategory] = React.useState('clothing-accessories');
+    const [totalCost, setTotalCost] = React.useState('');
+    const [stockType, setStockType] = React.useState('');
+    const [ideaType, setIdeaType] = React.useState('idea');
+    const [publicFiles, setPublicFiles] = React.useState();
+    const [privateFiles, setPrivateFiles] = React.useState();
     useEffect(() => {
     }, []);
 
@@ -21,7 +29,7 @@ const GetStarted = (props) => {
             backgroundSize: 'cover',
             backgroundPosition: 'top-center',
             backgroundRepeat: 'no-repeat',
-            width: `calc(100vw + 40px)`,
+            // width: `calc(100vw + 40px)`,
             // width: "100%",
             // margin: 0,
             padding: 0,
@@ -37,48 +45,62 @@ const GetStarted = (props) => {
         <div>
             <NavBar loggedIn={props.loggedIn} handleLogout={props.handleLogout} roles={props.roles} />
             <div>
-                <Container style={{ margin: 0, padding: 0 }}>
 
-                    <Grid container spacing={0} direction="column" >{/*All page content */}
+                <Grid container spacing={0} direction="column" >{/*All page content */}
 
-                        {/*Top section */}
-                        <Grid item style={styles.topSectionBackground}>
-                            <Box m={4} alignItems='center' justifyContent='center'>
-                                <Grid item container xs={12} >
-                                    <Grid item xs={false} md={3}>
+                    {/*Top section */}
+                    <Grid item style={styles.topSectionBackground}>
+                        <Box m={4} alignItems='center' justifyContent='center'>
+                            <Grid item container xs={12} >
+                                <Grid item xs={false} md={3}>
 
-                                    </Grid>
-                                    <Grid item xs={12} md={6}>
-                                        <Typography variant='h2'>Share your idea with the world</Typography>
-                                        <Typography variant='h5'>Follow the quick steps below to get your idea onto Sckedio.</Typography>
-                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12} md={6}>
+                                    <Typography variant='h2'>Share your idea with the world</Typography>
+                                    <Typography variant='h5'>Follow the quick steps below to get your idea onto Sckedio.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                    {/* Bottom section */}
+                    <Grid item container>
+
+                        <Grid item xs={12} md={4}>
+                            <Box margin={5}>
+                                <Typography variant='h3'>Get your idea into the world!</Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <Box m={10}>
+                                <Grid item container>
+                                    <GetStartedForm 
+                                        ideaName={ideaName}
+                                        setIdeaName={setIdeaName}
+                                        description={description}
+                                        setDescription={setDescription}
+                                        productCategory={productCategory}
+                                        setProductCategory={setProductCategory}
+                                        totalCost={totalCost}
+                                        setTotalCost={setTotalCost}
+                                        stockType={stockType}
+                                        setStockType={setStockType}
+                                        ideaType={ideaType}
+                                        setIdeaType={setIdeaType}
+                                        publicFiles={publicFiles}
+                                        setPublicFiles={setPublicFiles}
+                                        privateFiles={privateFiles}
+                                        setPrivateFiles={setPrivateFiles}
+                                    />
                                 </Grid>
                             </Box>
 
                         </Grid>
 
-                        {/* Bottom section */}
-                        <Grid item container>
-
-                            <Grid item xs={12} md={4}>
-                                <Box margin={5}>
-                                    <Typography variant='h3'>Get your idea into the world!</Typography>
-                                </Box>
-                            </Grid>
-                            <Grid item xs={12} md={8}>
-                                <Box m={10}>
-                                    <Grid item container>
-                                        <GetStartedForm />
-                                    </Grid>
-                                </Box>
-
-                            </Grid>
-
-                        </Grid>
-
                     </Grid>
 
-                </Container>
+                </Grid>
 
             </div>
         </div>
--- resources/js/react-app-project/pages/home/Home.js
+++ resources/js/react-app-project/pages/home/Home.js
@@ -13,7 +13,7 @@ import { useHistory } from "react-router-dom";
 const Home = (props) => {
 
     useEffect(() => {
-        
+
     }, []);
 
     let history = useHistory();
@@ -25,7 +25,7 @@ const Home = (props) => {
             backgroundSize: 'cover',
             backgroundPosition: 'top-center',
             backgroundRepeat: 'no-repeat',
-            width: `calc(100vw + 40px)`,
+            // width: `calc(100vw + 40px)`,
             // width: "100%",
             // margin: 0,
             padding: 0,
@@ -46,107 +46,92 @@ const Home = (props) => {
             backgroundSize: 'cover',
             backgroundPosition: 'bottom-center',
             backgroundRepeat: 'no-repeat',
-            width: `calc(100vw + 40px)`,
+            // width: `calc(100vw + 40px)`,
             padding: 0,
         }
     }
+
+    // return (
+    //     <div>
+    //         <NavBar loggedIn={props.loggedIn} handleLogout={props.handleLogout} roles={props.roles} />
+
+    //             <h1>Home</h1>
+    //             <Grid container spacing={0} >
+    //                 <Grid item style={styles.topSectionBackground} />
+
+    //             </Grid>
+    //     </div>
+    // );
 
     return (
         <div>
             <NavBar loggedIn={props.loggedIn} handleLogout={props.handleLogout} roles={props.roles} />
             {/* <h1>Home Page</h1> */}
             <div>
-                <Container style={{ margin: 0, padding: 0 }}>
 
-                    <Grid container spacing={0} direction="column" >{/*All page content */}
+                <Grid container spacing={0} direction="column" >{/*All page content */}
 
-                        {/*Top section */}
-                        <Grid item style={styles.topSectionBackground}>{/*Photo background and container of top panel of marketplace text+smaller image*/}
-                            <Box m={4}>
-                                <Grid item container xs={12}>{/*Container of "The marketplace..." text and image on right */}
-                                    <Grid item xs={12} md={6}>{/*The marketplace for ideas...+Sckedio connects... */}
-                                        <Typography variant='h2'>The marketplace for ideas</Typography>
-                                        <Typography variant='h5'>Sckedio connects people who have an idea for a product with people who want to build and buy their idea.</Typography>
-                                        <Box align="center">
-                                            <Button variant="contained" color="primary" onClick={()=>history.push('/get-started')}>Submit an idea</Button>
-                                        </Box>
-                                    </Grid>
-                                    <Grid item xs={12} md={6}>{/* Small image of man at whiteboard*/}
-                                        <Box display="flex" justifyContent="center" alignItems="center" backgroundSize="contain">
-                                            <img src={require("../../images/pexels-christina-morillo-1181345.jpg")} style={styles.smallWhiteboardImage} id="whiteboard-small-image" width="100%" border="100px" alt="whiteboard-small-image" />
-                                        </Box>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-
-                            <Box m={4}>
-                                <Grid item container xs={12} >
-                                    <Grid item xs={12} md={6}>
-                                        <Typography variant='h3'>Your ideas into action.</Typography>
-                                        <Typography variant='h5'>Sckedio connects people who have an idea for a product with people who want to build and buy their idea.</Typography>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Grid>
-
-                        {/*Middle section*/}
-                        <Grid item style={styles.middleSectionBackground}>
-
-                            <Grid item container>
-
-                                <Grid item xs={12} md={6}>{/* Image of artisan drawing on whiteboard*/}
-                                    <Box display="flex" justifyContent="center" alignItems="center" backgroundSize="contain">
-                                        <img src={require("../../images/artisan.jpg")} style={styles.artisanWhiteboardImage} id="artisan-image" width="100%" alt="artisan-image" />
+                    {/*Top section */}
+                    <Grid item style={styles.topSectionBackground}>{/*Photo background and container of top panel of marketplace text+smaller image*/}
+                        <Box m={4}>
+                            <Grid item container xs={12}>{/*Container of "The marketplace..." text and image on right */}
+                                <Grid item xs={12} md={6}>{/*The marketplace for ideas...+Sckedio connects... */}
+                                    <Typography variant='h2'>The marketplace for ideas</Typography>
+                                    <Typography variant='h5'>Sckedio connects people who have an idea for a product with people who want to build and buy their idea.</Typography>
+                                    <Box align="center">
+                                        <Button variant="contained" color="primary" onClick={() => history.push('/get-started')}>Submit an idea</Button>
                                     </Box>
                                 </Grid>
+                                <Grid item xs={12} md={6}>{/* Small image of man at whiteboard*/}
+                                    <Box display="flex" justifyContent="center" alignItems="center"
+                                    // backgroundSize="contain"
+                                    >
+                                        <img src={require("../../images/pexels-christina-morillo-1181345.jpg")} style={styles.smallWhiteboardImage} id="whiteboard-small-image" width="100%" border="100px" alt="whiteboard-small-image" />
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                        </Box>
 
+                        <Box m={4}>
+                            <Grid item container xs={12} >
                                 <Grid item xs={12} md={6}>
-                                    <Typography variant='h2'>Turn your idea into a reality and make money in the process.</Typography>
-                                    <Typography variant='h5'>Any idea — fully developed or a shower thought— gives you a chance to make money</Typography>
-                                    <Typography variant='h5'>Follow a few easy steps to change the world with your idea</Typography>
-                                    <Button variant="contained" color="primary" onClick={()=>history.push('/get-started')}>Upload your idea now</Button>
-                                    <Grid item container>
-                                        <Grid item xs={4}>
-                                            <Typography variant='h5' align='center'>Easy upload process</Typography>
-                                        </Grid>
-                                        <Grid item xs={4}>
-                                            <Typography variant='h5' align='center'>Free to submit</Typography>
-                                        </Grid>
-                                        <Grid item xs={4}>
-                                            <Typography variant='h5' align='center'>All ideas welcome</Typography>
-                                        </Grid>
+                                    <Typography variant='h3'>Your ideas into action.</Typography>
+                                    <Typography variant='h5'>Sckedio connects people who have an idea for a product with people who want to build and buy their idea.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Grid>
 
+                    {/*Middle section*/}
+                    <Grid item style={styles.middleSectionBackground}>
+
+                        <Grid item container>
+
+                            <Grid item xs={12} md={6}>{/* Image of artisan drawing on whiteboard*/}
+                                <Box display="flex" justifyContent="center" alignItems="center"
+                                // backgroundSize="contain"
+                                >
+                                    <img src={require("../../images/artisan.jpg")} style={styles.artisanWhiteboardImage} id="artisan-image" width="100%" alt="artisan-image" />
+                                </Box>
+                            </Grid>
+
+                            <Grid item xs={12} md={6}>
+                                <Typography variant='h2'>Turn your idea into a reality and make money in the process.</Typography>
+                                <Typography variant='h5'>Any idea — fully developed or a shower thought— gives you a chance to make money</Typography>
+                                <Typography variant='h5'>Follow a few easy steps to change the world with your idea</Typography>
+                                <Button variant="contained" color="primary" onClick={() => history.push('/get-started')}>Upload your idea now</Button>
+                                <Grid item container>
+                                    <Grid item xs={4}>
+                                        <Typography variant='h5' align='center'>Easy upload process</Typography>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Typography variant='h5' align='center'>Free to submit</Typography>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Typography variant='h5' align='center'>All ideas welcome</Typography>
                                     </Grid>
 
                                 </Grid>
-
-                            </Grid>
-
-                        </Grid>
-
-                        {/* Bottom section */}
-                        <Grid item container style={styles.bottomSectionBackground}>
-
-                            <Grid item xs={false} md={6}>
-
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Box m={10}>
-                                    <Grid item container>
-                                        <Grid item xs={6} md={12}>
-                                            <Typography variant='h4'>Stay tuned in.</Typography>
-                                            <Typography variant='h6'>Sckedio embraces the spirit of innovation, so make sure to keep up to date on our big changes</Typography>
-                                        </Grid>
-
-                                        <Grid item xs={6} md={12}>
-                                            <TextField id="form-name" label="Name" variant="outlined" />
-                                            <TextField id="form-email" label="Email" variant="outlined" />
-                                            <Button variant="contained" color="primary">Subscribe</Button>
-                                        </Grid>
-                                    </Grid>
-
-
-                                </Box>
 
                             </Grid>
 
@@ -154,7 +139,35 @@ const Home = (props) => {
 
                     </Grid>
 
-                </Container>
+                    {/* Bottom section */}
+                    <Grid item container style={styles.bottomSectionBackground}>
+
+                        <Grid item xs={false} md={6}>
+
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Box m={10}>
+                                <Grid item container>
+                                    <Grid item xs={6} md={12}>
+                                        <Typography variant='h4'>Stay tuned in.</Typography>
+                                        <Typography variant='h6'>Sckedio embraces the spirit of innovation, so make sure to keep up to date on our big changes</Typography>
+                                    </Grid>
+
+                                    <Grid item xs={6} md={12}>
+                                        <TextField id="form-name" label="Name" variant="outlined" />
+                                        <TextField id="form-email" label="Email" variant="outlined" />
+                                        <Button variant="contained" color="primary">Subscribe</Button>
+                                    </Grid>
+                                </Grid>
+
+
+                            </Box>
+
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
 
             </div>
         </div>
