839729945a1885f59634b20a57e4af714d50c001
Jarrett Dougherty <jarrettdougherty@gmail.com>
2021-03-31 21:38:43 -0400

    change Submit button

--- resources/js/react-app-project/pages/home/Home.js
+++ resources/js/react-app-project/pages/home/Home.js
@@ -156,7 +156,7 @@ const Home = (props) => {
                                     <Grid item container xs={12}>
                                         <Grid item xs={12}>
                                             <Box align="center">
-                                                <Button variant='contained' color='primary' onClick={() => history.push('/get-started')} >Submit an Idea</Button>
+                                                <Button variant='contained' color='primary' onClick={() => history.push('/sell')} >Submit an Idea</Button>
                                             </Box>
                                         </Grid>
                                         <Grid item xs={12}>
@@ -174,7 +174,7 @@ const Home = (props) => {
                                     <Grid item container xs={12} justify='space-between' >
                                         <Grid item>
 
-                                            <Button variant='contained' color='primary' onClick={() => history.push('/get-started')} >Submit an Idea</Button>
+                                            <Button variant='contained' color='primary' onClick={() => history.push('/sell')} >Submit an Idea</Button>
                                         </Grid>
                                         <Grid item>
 
